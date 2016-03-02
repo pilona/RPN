b28e1f9c4e88c2c16043b607fbf8a5ee3334c5b5
Alex Pilon <alp@alexpilon.ca>
2016-03-02 13:05:04 -0500

    Implement basic function arity dumping.

--- rpn.py
+++ rpn.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python3
 
 from sys import stdin
-from functools import reduce
+from inspect import signature as getsignature, Parameter
+from functools import reduce, wraps
+
+import inspect
 
 import operator
 
@@ -36,25 +39,44 @@ STR = r'''
          )
        )
        '''
+
+
+# FIXME: *really* dirty hack around inspect.signature not working on some
+# builtins.
+def unary(f):
+    # FIXME: @wraps(f) just exposes the original bug
+    #@wraps(f)
+    def wrapped(only):
+        return f(only)
+    return wrapped
+
+
+def binary(f):
+    #@wraps(f)
+    def wrapped(left, right):
+        return f(left, right)
+    return wrapped
+
+
 OPERATORS = {
     # Arithmetic
-    '+': operator.__add__,
-    '-': operator.__sub__,
+    '+': binary(operator.__add__),
+    '-': binary(operator.__sub__),
     # FIXME: Something better? Woulda been nice to keep this for floor
-    '_': operator.__neg__,
-    '*': operator.__mul__,
-    '/': operator.__truediv__,
-    '%': operator.__mod__,
-    '^': operator.__pow__,
+    '_': unary(operator.__neg__),
+    '*': binary(operator.__mul__),
+    '/': binary(operator.__truediv__),
+    '%': binary(operator.__mod__),
+    '^': binary(operator.__pow__),
 
     # Logical
-    '=': operator.__eq__,
-    '!': operator.__not__,
+    '=': binary(operator.__eq__),
+    '!': unary(operator.__not__),
 
     # Bitwise
-    '«': operator.__lshift__,
-    '»': operator.__rshift__,
-    '~': operator.__invert__,
+    '«': binary(operator.__lshift__),
+    '»': binary(operator.__rshift__),
+    '~': unary(operator.__invert__),
 }
 OPERATOR = r'(?:' + '|'.join(map(regex.escape, OPERATORS)) + r')'
 APPLY = r'\$'
@@ -93,15 +115,41 @@ def matchedgroups(match):
             if value and key != 'immediate'}
 
 
+def parse(match):
+    groups = matchedgroups(match)
+    if 'str' in groups:
+        yield groups['__str__']
+    elif 'number' in groups:
+        yield groups['number']
+    elif 'operator' in groups:
+        yield OPERATORS[groups['operator']]
+
+
+def arity(f):
+    if not callable(f):
+        return None
+    signature = inspect.signature(f)
+    parameters = signature.parameters.values()
+    positionals = [parameter
+                   for parameter
+                   in parameters
+                   if parameter.kind == Parameter.POSITIONAL_OR_KEYWORD and
+                      parameter.default == Parameter.empty]
+    return len(positionals)
+
+
 def dumper():
+    print('(<immediate?>) [groups] <repr> <arity>')
     for line in stdin:
         for match in lex(line):
             if isimmediate(match):
                 print('immediate', end=' ')
             matched = match.group(0)
             groups = matchedgroups(match).keys()
+            parsed = next(parse(match), None)
             print(*groups,
-                  repr(matched))
+                  repr(matched),
+                  arity(parsed))
 
 
 if __name__ == '__main__':
